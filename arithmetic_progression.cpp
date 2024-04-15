12508824d285e1a055220735cea9aedd90001a58
shruti2431 <166159752+shruti2431@users.noreply.github.com>
2024-04-15 20:24:29 +0530

    Add files via upload

---
+++ arithmetic_progression.cpp
@@ -0,0 +1,17 @@
+//Arithmetic progression 
+#include <iostream>
+using namespace std;
+
+int AP(int n)
+{
+    int ap=3*n+7;
+    return ap;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int ans=AP(n);
+    cout<<"Answer is="<<ans<<endl;
+    return 0;
+}
